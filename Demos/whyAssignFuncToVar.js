58da1c46134aa8375df555d9169f9fd1e5172c09
Samir70 <sbetmouni@talktalk.net>
2023-01-31 17:54:03 +0000

    format

--- Demos/whyAssignFuncToVar.js
+++ Demos/whyAssignFuncToVar.js
@@ -15,27 +15,27 @@ let divide = function (a, b) {
 };
 
 let ops = {
-    product: multiply,
-    sum: add, 
-    difference: difference,
-    quotient: divide
-}
+  product: multiply,
+  sum: add,
+  difference: difference,
+  quotient: divide,
+};
 
 let parseCalc = (str) => {
-    // str is something like "What is the product of 3 and 5"
-    words = str.split(" ")
-    op = words[3]
-    a = Number(words[5])
-    b = Number(words[7])
-    return `The ${words.slice(3).join(" ")} is ${ops[op](a, b)}`
-}
+  // str is something like "What is the product of 3 and 5"
+  words = str.split(" ");
+  op = words[3];
+  a = Number(words[5]);
+  b = Number(words[7]);
+  return `The ${words.slice(3).join(" ")} is ${ops[op](a, b)}`;
+};
 
 const tests = [
-    "What is the product of 3 and 5",
-    "What is the sum of 3 and 5",
-    "What is the difference of 3 and 5",
-    "What is the quotient of 3 and 5",
-    "What is the quotient of 35 and 5",
-]
+  "What is the product of 3 and 5",
+  "What is the sum of 3 and 5",
+  "What is the difference of 3 and 5",
+  "What is the quotient of 3 and 5",
+  "What is the quotient of 35 and 5",
+];
 
-tests.forEach(t => console.log(parseCalc(t)))
+tests.forEach((t) => console.log(parseCalc(t)));
