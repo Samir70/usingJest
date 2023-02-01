91ec2dc402da8bb2733fec1782402a0e30e6b7b5
Samir70 <sbetmouni@talktalk.net>
2023-02-01 17:29:11 +0000

    userbase example

---
+++ Userbase.js
@@ -0,0 +1,30 @@
+class User {
+  constructor(name) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getIntroduction() {
+    return `Hi, my name is ${this.name}`;
+  }
+}
+
+const users = [new User("Uma"), new User("Josh"), new User("Ollie")];
+
+class UserBase {
+  constructor(arrOfNames) {
+    this.users = arrOfNames;
+  }
+  count() {
+    return this.users.length;
+  }
+  getNames() {
+    return this.users.map((u) => u.getName());
+  }
+  getIntroductions() {
+    return this.users.map((u) => u.getIntroduction());
+  }
+}
